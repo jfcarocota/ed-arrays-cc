0ea653b933a14210c418b9d6a49a8f6f3cd0ef5e
jfcarocota <jfcarocota@gmail.com>
2020-08-25 07:55:42 -0700

    arrays memmory and for/foreach

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,33 @@
 
 int main()
 {
-    std::cout << "arrays" << std::endl;
+    int numbers[6]{100, 87982, 3456, 465, 885, 86};
+
+    std::cout << "Array memmory: " << numbers << std::endl;
+    std::cout << "Array memmory: " << *numbers << std::endl;
+
+    /*for(int i = 0; i < 6; i++)
+    {
+        std::cout << &numbers[i] << "\t";
+    }*/
+
+    /*for(int i = 0; i < 6; i++)
+    {
+        std::cout << numbers + i << "\t";
+    }*/
+
+    /*for(int n : numbers)
+    {
+        std::cout << &n  << "\t";
+    }*/
+
+    /*for(auto n : numbers)
+    {
+        std::cout << n  << "\t";
+    }*/
+
+
+    std::cout << std::endl;
 
     std::cin.get();
     
